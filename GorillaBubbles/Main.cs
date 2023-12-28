391ff12d3741b9639b0016b84455e8e6401242d8
OctoBurr <132259285+OctoBurr@users.noreply.github.com>
2023-12-28 15:25:49 +0000

    1.0.0
    added the sexy bubbles source code

---
+++ GorillaBubbles/Main.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using UnityEngine;
+namespace GorillaBubbles
+{
+    [BepInPlugin("OctoBurr.GorillaBubbles","GorillaBubbles","1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        GameObject bubbles;
+        void Update()
+        {
+            if (bubbles == null)
+                bubbles = GameObject.Find("Player Objects/Player VR Controller/GorillaPlayer/TurnParent/Main Camera/UnderwaterVisualEffects/UnderwaterParticleEffects/UnderwaterBubbles");
+            else
+            {
+                if (!bubbles.activeSelf)
+                {
+                    bubbles.SetActive(true);
+                    bubbles.transform.localPosition = new Vector3(0, 0.3f, 0);
+                }
+            }
+        }
+    }
+}
